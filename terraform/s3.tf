db80e46c0f18311c8b2decb2ecf841fdcb879b1f
Luiz Tadeu Mendonça <tadeu.mendonca@outlook.com>
2025-06-08 12:56:50 -0300

    FIX

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_object" "profile_content" {
   depends_on      = [aws_s3_bucket.profile]
   bucket = var.app_domain_profile
   key    = "index.html"
-  content = templatefile("${path.module}/../src/index.tpl",
+  content = templatefile("${path.module}/index.tpl",
   {
     AppName   = "LinkedIn"
     AppURL = "https://www.linkedin.com/in/luiz-tadeu-mendonca-83a16530"
@@ -38,7 +38,7 @@ resource "aws_s3_bucket_object" "wpp_content" {
   depends_on      = [aws_s3_bucket.wpp]
   bucket = var.app_domain_wpp
   key    = "index.html"
-  content = templatefile("${path.module}/../src/index.tpl",
+  content = templatefile("${path.module}/index.tpl",
   {
     AppName   = "WhatsApp Web"
     AppURL = "https://wa.me/5521986619954?text=Oi%20Tadeu%2C%20tudo%20bem%3F%20Gostaria%20de%20saber%20mais%20sobre%20a%20Transforma%C3%A7%C3%A3o%20Digital.%20Consegue%20me%20ajudar%3F"
@@ -62,7 +62,7 @@ resource "aws_s3_bucket_object" "insta_content" {
   depends_on      = [aws_s3_bucket.insta]
   bucket = var.app_domain_insta
   key    = "index.html"
-  content = templatefile("${path.module}/../src/index.tpl",
+  content = templatefile("${path.module}/index.tpl",
   {
     AppName   = "Instagram"
     AppURL = "https://www.instagram.com/tadeumen"
