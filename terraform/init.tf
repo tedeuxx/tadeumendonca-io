dd3afa884ec5160cb44100303b554a738736f2ff
Luiz Tadeu Mendonça <tadeu.mendonca@outlook.com>
2025-06-08 12:18:12 -0300

    FIX

--- terraform/init.tf
+++ terraform/init.tf
@@ -2,18 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
     }
   }
-
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "tadeumendonca"
-
-    workspaces {
-      name = "github"
-    }
-  }
+  cloud { 
+    organization = "tadeumendonca" 
+    workspaces { 
+      name = "github" 
+    } 
+  } 
 }
 
 # Configure the AWS Provider
